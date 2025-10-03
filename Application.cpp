c6565ecdec7005077f1a8294a3292e1f81dc4f5f
HEROWL <hiroakitogawa74@gmail.com>
2025-10-03 22:27:54 +0900

    Initial commit: Siv3D project setup

---
+++ Application.cpp
@@ -0,0 +1,25 @@
+﻿//Application.cpp
+#include "Application.hpp"
+
+Application::Application()
+{ }
+
+Application::~Application()
+{ }
+
+bool Application::init()
+{
+	Window::Resize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	return true;
+}
+
+void Application::update()
+{
+	//更新処理
+}
+
+void Application::draw()
+{
+	//描画処理
+	Circle{ 400,300,50 }.draw(Palette::Orange);
+}
