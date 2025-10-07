041eae9022f9eded4916fd61b39efc543a13216b
HazukiKojima <hazu0117a@gmail.com>
2025-10-07 19:09:47 +0900

    Add CharacterBase and Player implementation
    CharacterBase.cpp にアニメーション処理と描画処理を実装。
    Player.cpp を作成し、キー入力による移動とアニメーション切り替えを追加。
    
    AnimationInfo.hppでアニメーションの情報部分を定義。

---
+++ AnimationInfo.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+
+struct Animation
+{
+	int startFrame = 0; // アニメーションの開始フレーム
+	int endFrame = 0;   // アニメーションの終了フレーム
+	int row = 0;        // スプライトシートの行
+	Array<double> durations;
+};
+
+namespace AnimationKeys
+{
+	// Idle
+	const String Idle_D = U"idle_D";
+	const String Idle_U = U"idle_U";
+	const String Idle_L = U"idle_L";
+	const String Idle_R = U"idle_R";
+
+	// Walk
+	const String Walk_D = U"walk_D";
+	const String Walk_U = U"walk_U";
+	const String Walk_L = U"walk_L";
+	const String Walk_R = U"walk_R";
+}
---
+++ CharacterBase.hpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "AnimationInfo.hpp"
+#include <unordered_map>
+
+class CharacterBase
+{
+public:
+	CharacterBase(const Texture& texture);
+
+	virtual void Update();
+	virtual void Draw() const;
+
+	virtual void InitAnimation() {} // 継承先でアニメーション初期化
+	void AddAnimation(const String& name, const Animation& anim); // アニメーション登録
+	void PlayAnimation(const String& name); // アニメーション再生
+
+	// まだ未使用な部分が多い。将来的に使う予定
+	void SetPosition(const Vec2& pos) { m_position = pos; } // 位置設定
+	void SetVelocity(const Vec2& vel) { m_velocity = vel; } // 速度設定
+	Vec2 GetPosition() const { return m_position; } // 位置取得
+	Vec2 GetVelocity() const { return m_velocity; } // 速度取得
+
+protected:
+	String m_currentAnimationName; // 現在のアニメーション名
+	Vec2 m_position = Scene::Center(); // 位置
+	Vec2 m_velocity = { 0, 0 }; // 速度ベクトル
+	double m_moveSpeed = 2.0; // 移動速度
+private:
+
+	Texture m_texture; // スプライトシート
+	std::unordered_map<String, Animation> m_animations; // アニメーションリスト
+	double m_animationTimer = 0.0;
+	int m_currentFrame = 0;
+	int m_framewidth = 20; //1枚のフレームの幅
+	int m_frameheight = 28;
+};
