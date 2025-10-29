96989db6f0380cfcf5c2e41575d8cacecb6f2822
KR9827 <r.kajiwara0527@gmail.com>
2025-10-29 14:35:04 +0900

    Add Image
    ミニゲームできる絵の枚数を変更できるようにいました！

--- src/Scene/MiniGame/MiniGameScene_2.hpp
+++ src/Scene/MiniGame/MiniGameScene_2.hpp
@@ -4,13 +4,36 @@
 #include "../SceneTransition.hpp"
 #include "../MiniGame/MiniGameScene_2/Puzzle.hpp"
 
+enum class State
+{
+	Idle,
+	Playing,
+	Clear,
+	Finish,
+};
+
 class MiniGameScene_2 : public App::Scene
 {
 private:
 	std::unique_ptr<Puzzle> m_puzzle;
 
-	Font m_bigFont;
-	Vec2 m_fontPos;
+	State m_state;
+	Array<FilePath> m_puzzleImages;					// 使う画像たちを格納する
+	int32 m_puzzleIndex;							// パズルの画像の番号
+	double m_timer;									// 次の画像に推移するまでの時間
+
+	Font m_bigFont;									// 描画するフォント
+
+	void IdleUpdate();
+	void PlayingUpdate();
+	void ClearUpdate();
+	void FinishUpdate();
+
+	void IdleDraw() const;
+	void PlayingDraw() const;
+	void ClearDraw() const;
+	void FinishDraw() const;
+
 public:
 	MiniGameScene_2(const InitData& init);
 	~MiniGameScene_2();
--- src/Scene/MiniGame/MiniGameScene_2/Puzzle.hpp
+++ src/Scene/MiniGame/MiniGameScene_2/Puzzle.hpp
@@ -10,13 +10,15 @@ public:
 	~Puzzle() = default;
 
 	bool SystemInit();			// 重いリソースの初期化
-	void GameInit();			// ゲーム開始時の初期化
+	//void GameInit();			// ゲーム開始時の初期化
+	void GameInit(const FilePath& path);
 	void Update();
 	void Draw() const;
 
 	bool IsClear();
 	void ShufflePieces();
 	void ResetPuzzle();
+	const Texture& GetPrevTexture() const { return m_previewTex; }
 
 private:
 
